c4fe3622563b1e76502b19c82bc7ec2a89d2354c
Kartikey bhatnagar <kartikeybhatnagar247@gmail.com>
2024-12-14 17:27:13 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./Components/Navbar";
+import SideBar from "./Components/SideBar";
+
+function App() {
+  return (
+  <div>
+    <Navbar/>
+    <SideBar/>
+  </div>
+  )
+}
+
+export default App;
