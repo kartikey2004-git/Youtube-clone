6ccbc92639a88f04f7dc3cd3539f7264316408bd
Kartikey bhatnagar <kartikeybhatnagar247@gmail.com>
2024-12-15 18:28:09 +0530

    functional commit

--- src/Components/Video.jsx
+++ src/Components/Video.jsx
@@ -6,10 +6,10 @@ function Video({video}) {
   console.log(video);
   
   return (
-    <div className="ml-96zzz">
+    <div className="">
       <Link to={`/video/${video?.videoId}`}>
       <div>
-        <div className="flex">
+        <div>
           <img src={video?.thumbnails[0]?.url} alt=""/>
         </div>
       </div>
