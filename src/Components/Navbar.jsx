3e71588dddd01950077e098a27a33c06dae1dca9
Kartikey bhatnagar <kartikeybhatnagar247@gmail.com>
2024-12-25 19:26:53 +0530

    change in api

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,8 +6,7 @@ import { CiSearch } from "react-icons/ci";
 import { IoMdMic } from "react-icons/io";
 import { RiVideoAddLine } from "react-icons/ri";
 import { AiOutlineBell } from "react-icons/ai";
-
-import model from "../../public/model.png";
+import logo from "../../public/logo.png"
 import profile from "../../public/profile.jpg";
 import { useNavigate } from "react-router-dom";
 function Navbar() {
@@ -29,7 +28,7 @@ function Navbar() {
     <div className="flex justify-between fixed top-0 w-[100%] bg-white px-6 py-2 ">
       <div className="flex items-center space-x-4  ">
         <AiOutlineMenu className="text-xl cursor-pointer" />
-        <img src={model} alt="" className="w-28 cursor-pointer" />
+        <img src={logo} alt="" className="w-28 cursor-pointer" />
       </div>
       <div className="flex w-[35%] items-center ">
         <div className="w-[100%] px-4 py-2 border-[1px] border-gray-400 rounded-l-full">
